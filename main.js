d08a343d561803b037792f454dfba3025c0b179b
Alex Gibson <a.gibson@me.com>
2010-12-23 20:00:43 +0000

    validate against JSLint

--- main.js
+++ main.js
@@ -8,67 +8,69 @@
  * http://miniapps.co.uk/license/
  *
  */
+ 
+/*global window, QUOTA_EXCEEDED_ERR, alert, confirm */
 
 var checklistApp = {
 
 //get all items and displays the list
 getAllItems: function() {
-   	
-   	var checklist = '',
-  		myArray = [],
-   		completed = 0, //total number of checked items
-   		total = 0, //total number of items in list
-   		listlength = localStorage.length-1, //the total number of items held in localStorage
-   		item = '';
-   		
-   	for (var i = 0; i <= listlength; i++) {
-    	item = localStorage.key(i);
-    	myArray.push(item);
-    }
-    
-    myArray.sort();
-    
-    for (var j = 0; j <= listlength; j++) {
-    
-    	var sorteditem = myArray[j];
-    	
-    	//check item is not temporary (i.e. stored on key-up from user input)
-    	if (sorteditem !== 'inputvalue') {
-    	
-    		//is the item checked
-     		if (localStorage.getItem(sorteditem) === '1') {
-     		
-     			checklist += '<li role="listitem"><input type="checkbox" checked/><label class="done">' + sorteditem + '</label><span class="delete" tabindex="0">&times;</span></li>';
-     			completed ++;
-     			total ++;
-     		}
-     		else if (localStorage.getItem(sorteditem) === '0') {
-     		
-     			checklist += '<li role="listitem"><input type="checkbox" /><label>' + sorteditem + '</label><span class="delete" tabindex="0">&times;</span></li>';
-     			total ++;
-     		}
-     	}
-     	else {
-     		document.querySelector('#name').value = localStorage.getItem(sorteditem);
-     	}
-   	}
-   	
-   	if (checklist === '') {
-   		checklist = '<li role="listitem" class="empty">List empty</li>';
-   	}
-   	
-   	//update totals
-   	document.querySelector('#remaining').innerHTML = total - completed;
-   	document.querySelector('#total').innerHTML = total;
-   	
-   	//if all items are completed and the mail list button is showing, hide it.
-   	if (completed === total) {
-   		document.querySelector('#maillist').style.display = 'none';
-   	}
-   	
-   	//update view
-   	document.querySelector('#checklist').innerHTML = checklist;
-   	document.querySelector('#checklist').lastChild.style.border = '0';
+
+	var checklist = '',
+		myArray = [],
+		completed = 0, //total number of checked items
+		total = 0, //total number of items in list
+		listlength = window.localStorage.length-1, //the total number of items held in localStorage
+		item = '';
+
+	for (var i = 0; i <= listlength; i++) {
+		item = window.localStorage.key(i);
+		myArray.push(item);
+	}
+
+	myArray.sort();
+
+	for (var j = 0; j <= listlength; j++) {
+
+		var sorteditem = myArray[j];
+
+		//check item is not temporary (i.e. stored on key-up from user input)
+		if (sorteditem !== 'inputvalue') {
+
+			//is the item checked
+			if (window.localStorage.getItem(sorteditem) === '1') {
+
+				checklist += '<li role="listitem"><input type="checkbox" checked/><label class="done">' + sorteditem + '</label><span class="delete" tabindex="0">&times;</span></li>';
+				completed ++;
+				total ++;
+			}
+			else if (window.localStorage.getItem(sorteditem) === '0') {
+
+				checklist += '<li role="listitem"><input type="checkbox" /><label>' + sorteditem + '</label><span class="delete" tabindex="0">&times;</span></li>';
+				total ++;
+			}
+		}
+		else {
+			document.querySelector('#name').value = window.localStorage.getItem(sorteditem);
+		}
+	}
+
+	if (checklist === '') {
+		checklist = '<li role="listitem" class="empty">List empty</li>';
+	}
+
+	//update totals
+	document.querySelector('#remaining').innerHTML = total - completed;
+	document.querySelector('#total').innerHTML = total;
+
+	//if all items are completed and the mail list button is showing, hide it.
+	if (completed === total) {
+		document.querySelector('#maillist').style.display = 'none';
+	}
+
+	//update view
+	document.querySelector('#checklist').innerHTML = checklist;
+	document.querySelector('#checklist').lastChild.style.border = '0';
 },
 
 //capture click events in list
@@ -79,21 +81,27 @@ eventDelegation: function() {
 	listitems.onclick = function (e) {
 	
 		var event = e,
-			target = event.target;
+			target = event.target,
+			name = '',
+			data = 0;
 			
 		//<label>	
 		if(target.tagName.toLowerCase() === 'label') {
 		
-			var name = target.innerHTML,
-				data;
+			name = target.innerHTML;
 			
-			target.previousSibling.checked ? data = 0 : data = 1;
+			if (target.previousSibling.checked) {
+				data = 0;
+			}
+			else {
+				data = 1;
+			}
 
 			try {
-				localStorage.setItem(name, data);
-			} catch (e) {
+				window.localStorage.setItem(name, data);
+			} catch (err) {
 			
-				if (e == QUOTA_EXCEEDED_ERR) {
+				if (err == QUOTA_EXCEEDED_ERR) {
 					alert('Quota exceeded!');
 				}
 			}
@@ -102,16 +110,20 @@ eventDelegation: function() {
 		//<input>
 		else if(target.tagName.toLowerCase() === 'input') { 
 		
-			var name = target.nextSibling.innerHTML,
-				data;
+			name = target.nextSibling.innerHTML;
 				
-			target.checked ? data = 1 : data = 0;
+			if (target.checked) {
+				data = 1;
+			}
+			else {
+				data = 0;
+			} 
 
 			try {
-				localStorage.setItem(name, data);
-			} catch (e) {
+				window.localStorage.setItem(name, data);
+			} catch (error) {
 			
-				if (e == QUOTA_EXCEEDED_ERR) {
+				if (error == QUOTA_EXCEEDED_ERR) {
 					alert('Quota exceeded!');
 				}
 			}
@@ -120,14 +132,15 @@ eventDelegation: function() {
 		//<span>
 		else if(target.tagName.toLowerCase() === 'span') {
 		
-			var name = target.previousSibling.innerHTML;
+			name = target.previousSibling.innerHTML;
+			
 			if (!confirm("Delete item: " + name + "?")) {
 				return;
 			}
-			localStorage.removeItem(name);
+			window.localStorage.removeItem(name);
 			checklistApp.getAllItems();
 		}
-	}
+	};
 },
 
 //adds a single item
@@ -149,35 +162,35 @@ addNewItem: function() {
 	if (strippedString !== "") {
 	
 		try {
-			localStorage.setItem(strippedString, data);
+			window.localStorage.setItem(strippedString, data);
 		} catch (e) {
 			if (e == QUOTA_EXCEEDED_ERR) {
 				alert('Quota exceeded!');
 			}
 		}
 		//clear the input value
-   		document.querySelector('#name').value = "";
-   		//remove temporary item from storage
-   		localStorage.removeItem('inputvalue');
-   		checklistApp.getAllItems();
-   }
-   else {
-   		alert('Nothing to add!');
-   }
+		document.querySelector('#name').value = "";
+		//remove temporary item from storage
+		window.localStorage.removeItem('inputvalue');
+		checklistApp.getAllItems();
+	}
+	else {
+		alert('Nothing to add!');
+	}
 },
 
 //delete all checked items
 deleteChecked: function() {
 
-	if (!confirm("Delete checked items?")) return;
+	if (!confirm("Delete checked items?")) { return; }
 
 	var i = 0,
-		length = localStorage.length - 1;
+		length = window.localStorage.length - 1;
 			
 	while (i <= length) {		
-		var key = localStorage.key(i);
-		if (localStorage.getItem(key) === '1') {
-			localStorage.removeItem(key);
+		var key = window.localStorage.key(i);
+		if (window.localStorage.getItem(key) === '1') {
+			window.localStorage.removeItem(key);
 		}
 		else { i++; }
 	}
@@ -187,9 +200,9 @@ deleteChecked: function() {
 //delete all items
 deleteAll: function() {
 
-	if (!confirm("Delete all items?")) return;
+	if (!confirm("Delete all items?")) { return; }
 
-	localStorage.clear();
+	window.localStorage.clear();
 	checklistApp.getAllItems();
 },
 
@@ -202,18 +215,19 @@ updateMail: function() {
 	var mail = 'mailto:?',
 		subject = 'List',
 		list = '',
-		length = localStorage.length,
-		key = 0;
+		length = window.localStorage.length,
+		key = 0,
+		i = 0;
 	
 	if (length !== 0) {
 	
 		for (i = 0; i <= length-1; i++) {
 		
-			key = localStorage.key(i);
+			key = window.localStorage.key(i);
 			
 			//append to list if not checked
-			if (localStorage.getItem(key) === '0') {
-				list += localStorage.key(i) + '\n';
+			if (window.localStorage.getItem(key) === '0') {
+				list += window.localStorage.key(i) + '\n';
 			}
 		}
 
@@ -231,20 +245,25 @@ sendMail: function() {
 	var remaining = document.querySelector('#remaining').innerHTML,
 		maillist = document.querySelector('#maillist');
 		
-   	if (remaining !== '0') {
-   	
-   		checklistApp.updateMail();
+	if (remaining !== '0') {
 
-   		maillist.style.display === 'none' ? maillist.style.display = 'block' : maillist.style.display = 'none';
-   	}
-   	else {
-   		alert("You have no items to mail!");
-   	}
+		checklistApp.updateMail();
+		
+		if (maillist.style.display === 'none') {
+			maillist.style.display = 'block';
+		}
+		else {
+			maillist.style.display = 'none';
+		}
+	}
+	else {
+		alert("You have no items to mail!");
+	}
 },
 
 //store temporary item in localStorage
 storeInput: function() {
-	localStorage.setItem('inputvalue', this.value);
+	window.localStorage.setItem('inputvalue', this.value);
 }
 
 };
@@ -255,10 +274,10 @@ function loaded() {
 	window.scrollTo(0,0);
 	
 	//hack to enable active pseudo selectors on buttons in mobile webkit
-	document.addEventListener("touchstart",new Function(),false);
+	document.addEventListener("touchstart", function() {},false);
 	
 	//check if the browser supports localStorage
-	if (typeof(localStorage) == 'undefined' ) {
+	if (typeof(window.localStorage) == 'undefined' ) {
 		alert('Your browser does not support HTML5 localStorage. Try upgrading.');
 	} 
 	else {
